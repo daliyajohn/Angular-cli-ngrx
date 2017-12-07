a10571a48ff27fefcfcbd011555b8559b1b8b4b1
daliyajohn <daliyajohn75@gmail.com>
2017-12-07 15:39:12 +0530

    Add property add component

---
+++ src/app/container/property-details/property-add.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-add',
+  templateUrl: './property-add.component.html',
+  styleUrls: ['./property-add.component.css']
+})
+export class PropertyAddComponent implements OnInit {
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/container/property-details/property-add.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PropertyAddComponent } from './property-add.component';
+import { PropertyAddRoutingModule } from './property-add-routing.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatSelectModule,
+    PropertyAddRoutingModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  declarations: [ PropertyAddComponent ],
+  exports: [ PropertyAddComponent ],
+})
+export class PropertyAddModule { }
