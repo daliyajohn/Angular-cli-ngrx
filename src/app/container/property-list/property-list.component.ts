10bef59a23fd68ee7ae9c5e6058bcd7ccee546a8
daliyajohn <daliyajohn75@gmail.com>
2017-12-08 09:21:54 +0530

    property list UI fix

--- src/app/container/property-list/property-list.component.ts
+++ src/app/container/property-list/property-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-property-details',
+  selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
+
   constructor() { }
 
   ngOnInit() {
--- src/app/container/property-list/property-list.module.ts
+++ src/app/container/property-list/property-list.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PropertyListComponent } from './property-list.component';
+
 import { PropertyListRoutingModule } from './property-list-routing.module';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+import { PropertyListComponent } from './property-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatInputModule,
-    MatSelectModule,
-    PropertyListRoutingModule,
-    FlexLayoutModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatButtonModule
+    PropertyListRoutingModule
   ],
-  declarations: [ PropertyListComponent ],
-  exports: [ PropertyListComponent ],
+  declarations: [PropertyListComponent],
+  exports: [PropertyListComponent]
 })
 export class PropertyListModule { }
