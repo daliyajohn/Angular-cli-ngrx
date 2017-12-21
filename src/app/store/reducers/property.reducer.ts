1f56268540dcbad5bbcc82752498d97c1a6b220a
daliyajohn <daliyajohn75@gmail.com>
2017-12-21 10:51:04 +0530

    Add json

--- src/app/store/reducers/property.reducer.ts
+++ src/app/store/reducers/property.reducer.ts
@@ -6,31 +6,56 @@ import * as fromAction from '../actions/property.action';
 
 export interface PropertyState {
     dataReducer: PropertyModel[];
+    loaded: boolean;
+    loading: boolean;
 }
 
-export function propertyReducer (state, action): PropertyState {
-    console.log(action.payload);
+export const initialState: PropertyState = {
+    dataReducer: [  {
+        'id': 2,
+        'propertyname': 'daliya',
+        'email': 'da@gmail.com',
+        'phonenumber': 111111111,
+        'city': 'kundara',
+        'district': 'kollam',
+        'state': 'kerala',
+      }],
+    loaded: false,
+    loading: false
+ };
+
+export function propertyReducer (state = initialState, action: fromAction.propertyAction ): PropertyState {
     switch (action.type) {
         case fromAction.SUBMIT:
         return {
             ...state,
+            loaded: false,
+            loading: true,
             dataReducer: action.payload
         };
         case fromAction.SUBMIT_SUCCESS:
             return {
                 ...state,
+                loaded: true,
+                loading: false,
                 dataReducer: action.payload
             };
 
         case fromAction.SUBMIT_FAILURE :
         return {
             ...state,
+            loaded: false,
+            loading: false,
             dataReducer: action.payload
         };
     }
     return state;
 }
 
+
+
+export const getDataLoading = (states: PropertyState) => states.loading;
+export const getDataLoaded = (states: PropertyState) => states.loaded;
 export const getData = (states: PropertyState) => states.dataReducer;
 
 
